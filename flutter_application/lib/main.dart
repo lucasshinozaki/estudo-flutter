631374701dd893f7c8a38e9c4bd1369508952f4f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-11-01 15:59:04 -0300

    Iniciando estudo em flutter

---
+++ flutter_application/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplicativo Imc",
+      theme: ThemeData(
+        primarySwatch: Colors.blue        
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
