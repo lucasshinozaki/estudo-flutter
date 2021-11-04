75aa044614bbab8cb05dde9ec84970e3f57242ca
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-11-04 16:35:45 -0300

    estudo: Scaffold

--- estudo_flutter_application/lib/home_page.dart
+++ estudo_flutter_application/lib/home_page.dart
@@ -11,16 +11,27 @@ class HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
           child: GestureDetector(
-        child: Text("Contador: $counter"),
+        child: Text("Contador: $counter", style: TextStyle(fontSize: 20)),
         onTap: () {
           setState(() {
             counter++;
           });
         },
       )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
     );
   }
 }
