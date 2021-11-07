79f1f80b53306832552e967e719450f5da1aa3d8
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-11-07 15:05:30 -0300

    estudo: Columns

--- estudo_flutter_application/lib/home_page.dart
+++ estudo_flutter_application/lib/home_page.dart
@@ -20,8 +20,16 @@ class HomePageState extends State<HomePage> {
           CustomSwitch(),
         ],
       ),
-      body: Center(
-        child: CustomSwitch(),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Contador: $counter"),
+            CustomSwitch(),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
