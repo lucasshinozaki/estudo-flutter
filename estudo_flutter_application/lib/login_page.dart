b95271c4131b1755730b4580c738338e5c91e04f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-11-08 12:37:14 -0300

    estudo: Adding internet image

--- estudo_flutter_application/lib/login_page.dart
+++ estudo_flutter_application/lib/login_page.dart
@@ -23,6 +23,14 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Container(
+                width: 200,
+                height: 200,
+                child: Image.network(
+                  'https://cdn.logo.com/hotlink-ok/logo-social.png'
+                ),
+              ),
+              Container(height: 20),
               TextField(
                 onChanged: (text) {
                   email = text;
