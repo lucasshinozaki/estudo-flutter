3ee0efd5df81219158ccfa4293c6e71ec56a7f9b
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-11-09 15:38:55 -0300

    currency converte: Aligning view items

--- currency_converter_flutter_application/lib/app/views/home_view.dart
+++ currency_converter_flutter_application/lib/app/views/home_view.dart
@@ -9,26 +9,65 @@ class HomeView extends StatelessWidget {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Image.asset('assets/image/currency-converter-logo.png'),
-            SizedBox(
-             width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Expanded(child: 
-                  DropdownButton(items: null,onChanged: (value) {},)
-                  ),
-                  Expanded(
-                    child: TextField(
-                    decoration: InputDecoration(),
-                  ),
-                  )
-                ],
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/image/currency-converter-logo.png',
+                width: 100,
+                height: 100,
               ),
-            ),
-            ElevatedButton(onPressed: () {}, child: Text('CONVERTER')),
-          ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                      height: 56,
+                      child: DropdownButton(
+                        isExpanded: true,
+                        underline: Container(
+                          height: 1,
+                          color: Colors.amber,
+                        ),
+                        items: const [
+                          DropdownMenuItem(
+                            value: 1,
+                            child: Text('Real'),
+                          ),
+                          DropdownMenuItem(
+                            value: 2,
+                            child: Text('Dolar'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    )),
+                    SizedBox(width: 10),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.amber))),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 25),
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: () {}, 
+                child: Text('CONVERTER')),
+            ],
+          ),
         ),
       ),
     );
